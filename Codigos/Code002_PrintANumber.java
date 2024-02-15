e47b2bab700387872962b81b06a95272f6f54a70
Danilo <daniloaraujomota@gmail.com>
2024-02-15 12:12:28 -0300

    .

--- Codigos/Code002_PrintANumber.java
+++ Codigos/Code002_PrintANumber.java
@@ -8,6 +8,15 @@ public class Code002_PrintANumber {
       System.out.println("What is your name?");
       String name = scanner.nextLine();
 
+      System.out.println("What is your favorite food? ");
+      String food = scanner.nextLine();
+
+      System.out.println("What is your age? ");
+      int age = scanner.nextInt();
+      scanner.nextLine();
+      
       System.out.println("Hello "+name);
+      System.out.println("Yout favorite food is: "+food);
+      System.out.println("You are age is: "+ age);
    }
 }
