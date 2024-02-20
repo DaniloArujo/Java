dc304f24ab04bf15caed9c1726d4cd3cdf7d9830
Danilo <daniloaraujomota@gmail.com>
2024-02-20 05:40:03 -0300

    .

---
+++ Codigos/Code023_OverloadMethods.java
@@ -0,0 +1,20 @@
+package Codigos;
+
+public class Code023_OverloadMethods {
+    public static void main(String[] args){
+        System.out.println(add(2,3));
+        System.out.println(add(2, 4, 8));
+    }
+    static int add(int a, int b){
+        return a + b;
+    }
+
+    static int add(int a, int b, int c){
+        return a + b + c;
+    }
+
+
+
+
+
+}
