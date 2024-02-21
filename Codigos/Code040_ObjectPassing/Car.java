5561827fd2bbb5478b70904d53707f9e76f78acc
Danilo <daniloaraujomota@gmail.com>
2024-02-20 23:07:47 -0300

    .

---
+++ Codigos/Code040_ObjectPassing/Car.java
@@ -0,0 +1,11 @@
+package Codigos.Code040_ObjectPassing;
+
+public class Car {
+    
+    String name;
+
+    Car(String name){
+        this.name = name;
+    }
+
+}
---
+++ Codigos/Code040_ObjectPassing/Garage.java
@@ -0,0 +1,8 @@
+package Codigos.Code040_ObjectPassing;
+
+public class Garage {
+
+    void park(Car car){
+      System.out.println("The "+car.name+" is parked in the garage.");  
+    }
+}
