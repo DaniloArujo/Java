3833001c51c074e9d0411e0c0d744a06fc64fa35
Danilo <daniloaraujomota@gmail.com>
2024-02-25 09:42:09 -0300

    .

---
+++ Codigos/Code046_AcessModifiers/acessModifiers.java
@@ -0,0 +1,31 @@
+package Codigos.Code046_AcessModifiers;
+
+public class acessModifiers {
+    /*
+     * Public (public):
+
+    Members declared as public are accessible from any other class.
+    There are no restrictions on accessing public members.
+     * 
+     * 
+     * Private (private):
+
+    Members declared as private are only accessible within the same class.
+    They are not visible to other classes.
+     * 
+     * 
+     * Protected (protected):
+
+    Members declared as protected are accessible within the same class, subclasses, and classes in the same package.
+    They are not accessible outside the package if the subclass is in a different package.
+
+     * 
+     * Default (Package-Private, no modifier):
+
+    If no access modifier is specified (default), the member is accessible within the same class and other classes in the same package.
+    It is not accessible outside the package.
+     * 
+     * 
+     * 
+     */
+}
